be6ed509dab67d37d784b704c8c42928b5830f38
Rohan Parmar <rohanrparmar987@gmail.com>
2022-08-02 19:55:37 +0530

    add views and urls

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,8 @@
+from http.client import HTTPResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home (request):
+    return HttpResponse("Hello I am Working :) ")
