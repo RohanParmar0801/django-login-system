3cfb7dab27360ce5e68869e679729db6dbcaf660
Rohan Parmar <rohanrparmar987@gmail.com>
2022-08-02 21:41:44 +0530

    add signup templates

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,4 +4,7 @@ from authentication import views as authentication_views
 
 urlpatterns = [
     path('',authentication_views.home, name='home'),
+    path('signup',authentication_views.signup, name='signup'),
+    path('signin',authentication_views.signin, name='signin'),
+    path('signout',authentication_views.signout, name='signout'),
 ]
